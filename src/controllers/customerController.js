b406822e003287772e9133bc07d455d1f60f8e84
SHUNNIORR <jorgeomarpp@ufps.edu.co>
2021-03-11 16:43:51 -0500

    Version 1.0

---
+++ src/controllers/customerController.js
@@ -0,0 +1,53 @@
+const controller = {};
+
+controller.list = (req,res)=>{
+    req.getConnection((err,con)=>{
+        con.query('SELECT * FROM customer', (err,customers)=>{
+            if(err){
+                res.json(err);
+            }
+            res.render('customers',{
+                data: customers
+            });
+        });
+    });
+};
+
+controller.save = (req,res)=>{
+    const data= req.body;
+    req.getConnection((err, con)=>{
+        con.query('INSERT INTO customer set ?',[data], (err,customer)=>{
+            res.redirect('/');
+        });
+    });
+};
+controller.delete = (req,res)=>{
+    const {id} = req.params;
+    req.getConnection((err, con)=>{
+        con.query('DELETE FROM customer WHERE id=?',[id],(err, rows)=>{
+            res.redirect('/');
+        })
+    })  
+};
+
+controller.edit = (req,res)=>{
+    const {id} = req.params;
+    req.getConnection((err, con)=>{
+        con.query('SELECT * FROM customer WHERE id=?',[id],(err, customer)=>{
+            res.render('customer_edit',{
+                data: customer[0]
+            })
+        })
+    })  
+};
+controller.update = (req,res)=>{
+    const {id} = req.params;
+    const newCustomer= req.body;
+    req.getConnection((err, con)=>{
+        con.query('UPDATE customer set? WHERE id=?',[newCustomer,id],(err, rows)=>{
+            res.redirect('/');
+        })
+    })  
+};
+
+module.exports = controller;
